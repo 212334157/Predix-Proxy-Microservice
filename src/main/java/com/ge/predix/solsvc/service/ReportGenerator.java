483e5cd52093534c8183fe35d04ebb48faf398f8
joshua.panka@ge.com <joshua.panka@ge.com>
2016-05-10 12:16:21 -0500

    remove files

--- src/main/java/com/ge/predix/solsvc/service/ReportGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.ge.predix.solsvc.service;
-
-public interface ReportGenerator {
-	String displayReportTemplate(String reportFormat,int reportEntries);
-    String generateComplexReport(String reportFormat,int reportEntries);
-    String generateSensitiveReport();
-}
